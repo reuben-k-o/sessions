689d1eba43a32ea4242eba05c35e663f011ef686
reuben-k-o <rubenkhaemba@gmail.com>
2022-04-22 22:04:47 +0300

    mongoose setup

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
 
+const { MONGODB_URI } = require("./config");
 const app = express();
 
-mongoose.connect();
-app.listen(5000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(MONGODB_URI).then(() => {
+  app.listen(5000);
+});
